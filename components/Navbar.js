dd0c92ce71e2a7a976c133be10bf25c71e5aed39
kimyoosang <yoo99485@gmail.com>
2022-04-03 00:57:29 +0900

    [Add] requests.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,8 @@
+import { useRouter } from "next/router";
 import React from "react";
 
 export default function Navbar() {
+  const router = useRouter();
   return (
     <div>
       <h1>Navbar</h1>
