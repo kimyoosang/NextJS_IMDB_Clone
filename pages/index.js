62da3fbc61d7b7bd2a9a2562bc06c5c6b08376d0
kimyoosang <yoo99485@gmail.com>
2022-04-03 01:24:26 +0900

    [Update] add api get image to Card

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Header from "../components/Header";
 import Navbar from "../components/Navbar";
 import Results from "../components/Results";
+import requests from "../utils/requests";
 
 export default function Home({ results }) {
   return (
@@ -24,8 +25,8 @@ export default function Home({ results }) {
 export async function getServerSideProps(context) {
   const genre = context.query.genre || "fetchTrending";
   const request = await fetch(`
-  https://api.themoviedb.org/3${request[genre].url}
-  `).then((res) => res.json());
+  https://api.themoviedb.org/3${requests[genre].url}
+  `).then((response) => response.json());
 
   return {
     props: {
