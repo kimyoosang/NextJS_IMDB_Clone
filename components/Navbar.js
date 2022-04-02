90d9d77095bbd5c16f92b1be3fac1571f28d2a94
kimyoosang <yoo99485@gmail.com>
2022-04-03 01:04:51 +0900

    [Update] Navbar titles

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,21 @@
 import { useRouter } from "next/router";
+import requests from "../utils/requests";
+
 import React from "react";
 
 export default function Navbar() {
   const router = useRouter();
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className="flex justify-center bg-gray-600 text-gray-200 select-none text-xl lg:text-2xl">
+      {Object.entries(requests).map(([key, { title, url }]) => (
+        <h2
+          onClick={() => router.push(`?genre=${key}`)}
+          className="m-6 cursor-pointer hover:text-white active:text-red-400"
+          key={key}
+        >
+          {title}
+        </h2>
+      ))}
     </div>
   );
 }
