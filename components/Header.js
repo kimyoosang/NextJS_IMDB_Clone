e2dfa8457ab2e303c4b300ccfe2d1f8fbf4a9f1f
kimyoosang <yoo99485@gmail.com>
2022-04-03 00:23:09 +0900

    [Add] HeaderIcon component

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,18 @@
+import Image from "next/image";
 import React from "react";
 
 export default function Header() {
   return (
     <div>
-      <h1>Header</h1>
+      <div className="bg-gray-700 text-gray-200 flex flex-col items-center p-6 select-none sm:flex-row justify-between"></div>
+      <Image
+        src={
+          "https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg"
+        }
+        width={100}
+        height={100}
+        className="cursor-pointer active:brightness-110"
+      />
     </div>
   );
 }
